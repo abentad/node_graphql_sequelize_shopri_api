9f03f90927d5aca5f4df3bdd2d0cc91e1016d271
Abenezer <abentad4@gmail.com>
2021-11-27 23:58:13 +0300

    images type im schema added and images for product integrated

--- src/resolvers/Product.js
+++ src/resolvers/Product.js
@@ -3,6 +3,10 @@ const Product = {
     async poster(parent, args, { db }, info){
         const [user] = await db.users.findAll({ where: { id: parent.posterId}});
         return user;
+    },
+    async images(parent, args, { db }, info){
+        const images = await db.images.findAll({ where: { id: parent.id }});
+        return images;
     }
 };
 
